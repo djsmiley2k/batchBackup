5950ebb5e21de2fc67de982a46da50741712493f
Tim Bowers <djsmiley2k@gmail.com>
2017-05-16 11:01:36 +0100

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+
+:: FOR %%A in ('LIST_OF_IPs_COMMA_SEPERATED') DO net use \\%%A\c$ /delete & net use \\%%A\c$ /user:$username $password
+
+echo 'All drives mapped'
+
+echo 'Starting Copies'
+start "Name of Window" /D "script_path" backup.bat 
+
+
