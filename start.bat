6a3c8971286fc7e5a855446239c0cb9d0c985e57
Tim Bowers <djsmiley2k@gmail.com>
2017-07-07 23:17:07 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 
-:: FOR %%A in ('LIST_OF_IPs_COMMA_SEPERATED') DO net use \\%%A\c$ /delete & net use \\%%A\c$ /user:$username $password
+:: FOR %%A in (LIST_OF_IPs_COMMA_SEPERATED) DO net use \\%%A\c$ /delete & net use \\%%A\c$ /user:$username $password
 
 echo 'All drives mapped'
 
